7f3da412c9f1cd04bb6d1b101f6cd33714ff86aa
Adams Kiptalam <adamskiptalam0@gmail.com>
2024-04-20 13:35:16 +0300

    Update SnakeCase List Comprehension.py
    List Comprehension is a way to construct a new Python list from an iterable types: lists, tuples, and strings. 
    All without using a for loop or the `.append()` list method

--- SnakeCase List Comprehension.py
+++ SnakeCase List Comprehension.py
@@ -1,6 +1,8 @@
+'''List Comprehension is a way to construct a new Python list from an iterable types: lists, tuples, and strings. 
+    All without using a for loop or the `.append()` list method.'''
+
 #  A program that takes a string formatted in Camel Case or Pascal Case, then converts it into Snake Case, using "List Comprehension"
 
-    
 def convert_to_snake_case(pascal_or_camel_cased_string):
 
     snake_cased_char_list = [
